b31c839cac8b4ccd2978c1a7687d257ecf051a22
henrymbuguak <henrymbuguak@gmail.com>
2017-08-30 15:27:59 +0300

    First step with angular integration

--- scrumboard/urls.py
+++ scrumboard/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 from .api import ListApi, CardApi
+
 
 urlpatterns = [
     url(r'^lists$', ListApi.as_view()),
     url(r'^cards$', CardApi.as_view()),
+    url(r'^index', TemplateView.as_view(template_name="scrumboard/index.html")),
 ]
