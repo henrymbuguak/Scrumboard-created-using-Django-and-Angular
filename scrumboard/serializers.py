f258774729b830b25519d7d08f938516085ae2a2
henrymbuguak <henrymbuguak@gmail.com>
2017-08-30 13:21:47 +0300

    Django rest api framework implementation

---
+++ scrumboard/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from .models import List, Card
+
+class ListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = List
+        fields = ('name',)
+
+class CardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card
+        fields = ('title','description','list','story_points','business_value',)
