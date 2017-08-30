7837b6aa386001c674077827ca0885dd22f878eb
henrymbuguak <henrymbuguak@gmail.com>
2017-08-30 21:52:50 +0300

    Using ModelViewset and DefaultRouter-Rest framework

--- scrumboard/urls.py
+++ scrumboard/urls.py
@@ -1,3 +1,4 @@
+"""
 from django.conf.urls import url
 from django.views.generic import TemplateView
 from .api import ListApi, CardApi
@@ -8,3 +9,14 @@ urlpatterns = [
     url(r'^cards$', CardApi.as_view()),
     url(r'^index', TemplateView.as_view(template_name="scrumboard/index.html")),
 ]
+
+"""
+from .api import ListViewSet, CardViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register(r'lists',ListViewSet)
+router.register(r'cards',CardViewSet)
+
+urlpatterns = router.urls
