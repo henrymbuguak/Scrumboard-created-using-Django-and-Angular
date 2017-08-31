8bb234171e63b3142d885788654643b9d11a1ed2
henrymbuguak <henrymbuguak@gmail.com>
2017-08-31 11:07:57 +0300

    Angular js card implementantion

---
+++ scrumboard/static/scrumboard/card.directive.js
@@ -0,0 +1,11 @@
+(function () {
+    'use strict';
+    angular.module('scrumboard.demo').directive('scrumboardCard', CardDirective);
+    
+    function CardDirective() {
+        return {
+            templateUrl: '/static/scrumboard/card.html',
+            restrict: 'E'
+        };
+    }
+})();
