a0209f14eb1b47e25b8d3ba024c181604b9792e3
henrymbuguak <henrymbuguak@gmail.com>
2017-09-03 22:59:55 +0300

    Managing login status using angular custom services

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,9 @@
 (function () {
         'use strict';
 
-        angular.module('scrumboard.demo', ['ngRoute']).controller('ScrumboardController', ['$scope','$http', '$location',ScrumboardController]);
+        angular.module('scrumboard.demo', ['ngRoute']).controller('ScrumboardController', ['$scope','$http', 'Login','$location',ScrumboardController]);
 
-        function ScrumboardController($scope, $http, $location) {
+        function ScrumboardController($scope, $http, Login, $location) {
             $scope.add = function (list, title) {
                 var card = {
                     list: list.id,
@@ -16,17 +16,21 @@
                 });
             };
 
-            $scope.logout = function () {
+           /* $scope.logout = function () {
                 $http.get('/auth_api/logout/').then(function () {
                     $location.url('/login');
                 });
-            }
+            }*/
 
 
             $scope.data = [];
             $http.get('/scrumboard/lists/').then(function (response) {
                 $scope.data = response.data;
             });
+
+            Login.redirectIfNotLoggedIn();
+            $scope.data=[];
+            $scope.logout = Login.logout;
 
             $scope.sortBy='story_points';
             $scope.reverse=true;
--- static/js/login.controller.js
+++ static/js/login.controller.js
@@ -1,15 +1,19 @@
 (function () {
     'use strict';
-    angular.module('scrumboard.demo').controller('LoginController', ['$scope', '$http', '$location', LoginController]);
+    angular.module('scrumboard.demo').controller('LoginController', ['$scope', '$http', '$location','Login', LoginController]);
     
-    function LoginController($scope, $http, $location) {
+    function LoginController($scope, $http,$location, Login) {
         $scope.login = function () {
-            $http.post('/auth_api/login/', $scope.user).then(function () {
+            Login.login($scope.user).then(function () {
                 $location.url('/');
             },
             function () {
                 $scope.login_error='Invalid username or password';
             });
+        }
+
+        if (Login.isLoggedIn()) {
+            $location('/');
         }
     }
 })();
